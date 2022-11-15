2a1b97f1794a6d826bb108ff75b91ecac6145aad
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-11-15 15:57:58 +0600

    done

---
+++ cmd/main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/mdminhaziftekhar/Go-Bookstore/pkg/routes"
+)
+
+func main(){
+
+	r := mux.NewRouter()
+
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+	
+	
+}
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mdminhaziftekhar/Go-Bookstore/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+
+}
