070b4bab685bc8fcf785b99ec153b7a1a6aac142
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-11-16 11:30:44 +0600

    Updated

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -12,6 +12,8 @@ var (
 
 func Connect() {
 
+	// USERNAME:PASSWORD@tcp(127.0.0.1:3306)/database_name?charset=utf8&parseTime=True&loc=Local
+
 	d, err := gorm.Open("mysql", "moon:root@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
