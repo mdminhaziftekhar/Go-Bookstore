58f1c1d7665f56279d8060f1b8616584d0498d6f
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-11-20 11:38:44 +0600

    Added Yaml file

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,7 +1,12 @@
 package config
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
 	"github.com/jinzhu/gorm"
+	"gopkg.in/yaml.v3"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -10,11 +15,47 @@ var (
 	db *gorm.DB
 )
 
+type conf struct {
+	Encoding  string `yaml:"encoding"`
+	Reconnect bool   `yaml:"reconnect"`
+	TCP       string `yaml:"tcp"`
+	Database  string `yaml:"database"`
+	Username  string `yaml:"username"`
+	Password  string `yaml:"password"`
+}
+
+func (c *conf) getConf() *conf {
+
+	yamlFile, err := ioutil.ReadFile("conf.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	// fmt.Println("DEBUG ...")
+	// fmt.Println("Database : ", c.Database)
+	// fmt.Println("Username : ", c.Username)
+	// fmt.Println("Password : ", c.Password)
+	// fmt.Println("Reconnect : ", c.Reconnect)
+	// fmt.Println("Encoding : ", c.Encoding)
+
+	return c
+}
+
 func Connect() {
+
+	var c conf
+
+	c.getConf()
 
 	// USERNAME:PASSWORD@tcp(127.0.0.1:3306)/database_name?charset=utf8&parseTime=True&loc=Local
 
-	d, err := gorm.Open("mysql", "moon:root@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
+	loginCred := c.Username + ":" + c.Password + "@" + c.TCP + "/" + c.Database + "?" + c.Encoding
+
+	d, err := gorm.Open("mysql", loginCred)
 
 	if err != nil {
 		panic(err)
